46047eeee0b70f7cde3cb6191e64901ad1223286
Emilia <97615383+Pinkulani@users.noreply.github.com>
2024-08-01 19:54:04 +0200

    Adding exit option

--- Shell/Controller.py
+++ Shell/Controller.py
@@ -1,6 +1,9 @@
 from Shell.Help import Help
+from Shell.Exit import Exit
 
 def Controller(Command: str, Argument: str):
     match Command:
+        case "exit":
+            Exit()
         case "help":
             Help()
