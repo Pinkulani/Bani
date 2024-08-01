4f3b55080d511fd005aecad27bf9e65f60db5eb4
Emilia <97615383+Pinkulani@users.noreply.github.com>
2024-08-01 12:05:39 +0200

    Adding file for help command

---
+++ Shell/Help.py
@@ -0,0 +1,7 @@
+def Help():
+    print("Drzewo Shell")
+    print("")
+    print("exit - Exit the Shell")
+    print("help - Calls up the help menu")
+    print("info - Lists system specifications")
+    print("tools - Open built-in shell tools")
