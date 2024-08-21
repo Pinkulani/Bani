afdacc316d6bb73b58cbdae50f7e9453b7b2bb8d
Emilia <97615383+Pinkulani@users.noreply.github.com>
2024-08-21 06:25:32 +0200

    Delete Shell directory

--- Shell/Help.py
+++
@@ -1,7 +0,0 @@
-def Help():
-    print("Drzewo Shell")
-    print("")
-    print("exit - Exit the Shell")
-    print("help - Calls up the help menu")
-    print("info - Lists system specifications")
-    print("tools - Open built-in shell tools")
