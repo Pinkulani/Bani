603995a1d6dd5f8ba9ed9e05b99988754f637e56
Emilia <97615383+Pinkulani@users.noreply.github.com>
2024-08-23 06:15:16 +0200

    Adding function for splitting strings at space

--- String/Split.py
+++ String/Split.py
@@ -1,1 +1,12 @@
-def Split(String: str) -> str:
+def Split(Input: str) -> str:
+    Array = []
+    New = []
+    for X in range(len(Input)):
+        if Input[X] == " ":
+            Array.append(New)
+            New = []
+        else:
+            New.append(Input[X])
+
+    Array.append(New) # Append last letters without space
+    return Array
